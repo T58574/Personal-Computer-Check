e12742bdbd4333793cc33d8736a232e2f52e1bd5
T58 <64015619+T58574@users.noreply.github.com>
2022-02-25 03:12:07 +0500

    main

--- build.py
+++ build.py
@@ -1,3 +1,4 @@
+# It imports the modules that will be used in the script.
 import re
 import psutil
 import subprocess
@@ -8,6 +9,13 @@ import uuid
 
 def main():
     def get_size(bytes, suffix="B"):
+        """
+        Given a number of bytes, return a human readable string with the correct unit of measure
+
+        :param bytes: The number of bytes to convert
+        :param suffix: The suffix to use for the size, defaults to B (optional)
+        :return: a string with the size of the file in human-readable format.
+        """
         factor = 1024
         for unit in ["", "K", "M", "G", "T", "P"]:
             if bytes < factor:
@@ -15,12 +23,21 @@ def main():
             bytes /= factor
 
     def extract_wifi_passwords():
+        """
+        It extracts the wifi passwords from the system.
+        :return: a string.
+        """
         profiles_data = subprocess.check_output(
             'netsh wlan show profiles', encoding='CP866', errors='ignore').split('\n')
         profiles = [i.split(':')[1].strip()
                     for i in profiles_data if 'Все профили пользователей' in i]
 
         def for_pass():
+            """
+            It uses the subprocess module to run the command "netsh wlan show profile"
+                    and then parses the output to find the password.
+            :return: a string.
+            """
             arr = ["-------- Wifi Passwords --------"]
             for profile in profiles:
                 profile_info = subprocess.check_output(
@@ -34,6 +51,18 @@ def main():
             return ("\n".join(map(str, arr)))
         return for_pass()
 
+    # 1. uname = platform.uname()
+    #     2. cpufreq = psutil.cpu_freq()
+    #     3. svmem = psutil.virtual_memory()
+    #     4. cpu_information = (f'-------- System Information --------\nSystem: {uname.system}\nNode
+    # Name: {uname.node}\nRelease: {uname.release}\nVersion: {uname.version}\nMachine:
+    # {uname.machine}\nProcessor: {uname.processor}\nIp-Address:
+    # {socket.gethostbyname(socket.gethostname())}\nMac-Address: {":".join(re.findall("..", "%012x" %
+    # uuid.getnode()))}\n-------- CPU Information --------\nPhysical cores:
+    # {psutil.cpu_count(logical=False)}\nTotal cores: {psutil.cpu_count(logical=True)}\nMax Frequency:
+    # {cpufreq.max:.2f}Mhz')
+    #     5. memory_information = (
+    #         f
     uname = platform.uname()
     cpufreq = psutil.cpu_freq()
     svmem = psutil.virtual_memory()
@@ -41,10 +70,15 @@ def main():
     memory_information = (
         f'-------- Memory Information --------\nTotal: {get_size(svmem.total)}\nAvailable: {get_size(svmem.available)}')
 
+# A context manager.
     with open(file='full_info.txt', mode='a', encoding='utf-8') as file:
         file.write(
             f'{extract_wifi_passwords()}\n{cpu_information}\n{memory_information}')
 
 
+# This is a special variable that is created when you run a python file.
+# equal to the string `__main__` when the file is run directly,
+# but is equal to the name of the module that imported it when the file is imported.
+# This allows you to determine if a file is being run directly or being imported.
 if __name__ == '__main__':
     main()
